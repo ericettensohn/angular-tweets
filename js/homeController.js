0e0077eb0d0fafa19c1206800bdca32af1b9fccc
ericettensohn <ericettensohn@gmail.com>
2016-08-17 11:59:18 -0400

    3

--- js/homeController.js
+++ js/homeController.js
@@ -1,5 +1,21 @@
-tweetApp.controller('homeController', function($scope, $http){
+tweetApp.controller('homeController', function($scope, $http, $location){
 	$scope.homePage = true;
+
+	// $scope.$watch(function(){
+	// 	return $location.path();
+	// },
+	// function(newPath){
+	// 	console.log("The path changed. It is now " + newPath);
+	// 	$scope.trump = "";
+	// 	$scope.hillary = "";
+	// 	$scope.home = "";
+	// 	if (newPath == "/trump") {
+	// 		$scope.trump = "disabled";
+	// 	}
+	// 	else if (newPath == "/hillary") {
+	// 		$scope.trump = "disabled";
+	// 	}
+	// });
 
 
 });
